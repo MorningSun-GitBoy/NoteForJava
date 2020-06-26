a76a0662ec984b2c2a94d964b25e694c6a6ee0f2
Bachelor <AndyWolfCF2013@163.com>
2020-06-26 22:53:04 +0800

    new chapter

---
+++ 01Basic/src/object/Compute.java
@@ -0,0 +1,30 @@
+package object;
+
+public class Compute {
+	
+	private static int plus;
+	private static double plusd;
+	private static int minus;
+	private static double minusd;
+	private static int mutiple;
+	private static double mutipled;
+	public static int getPlus() {
+		return plus;
+	}
+	public static double getPlusd() {
+		return plusd;
+	}
+	public static int getMinus() {
+		return minus;
+	}
+	public static double getMinusd() {
+		return minusd;
+	}
+	public static int getMutiple() {
+		return mutiple;
+	}
+	public static double getMutipled() {
+		return mutipled;
+	}
+
+}
