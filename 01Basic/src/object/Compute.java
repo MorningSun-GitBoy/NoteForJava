ba2729ba55f3df2e410eb82fbe5ad6b151c8be78
Bachelor <AndyWolfCF2013@163.com>
2020-06-27 14:35:17 +0800

    finish compute

--- 01Basic/src/object/Compute.java
+++ 01Basic/src/object/Compute.java
@@ -2,12 +2,20 @@ package object;
 
 public class Compute {
 	
-	private static int plus;
-	private static double plusd;
-	private static int minus;
-	private static double minusd;
-	private static int mutiple;
-	private static double mutipled;
+	private static int plus = 0;
+	private static double plusd = 0;
+	private static int minus = 0;
+	private static double minusd = 0;
+	private static int mutiple = 0;
+	private static double mutipled = 0;
+	private static int delive = 0;
+	private static double delived = 0;
+	public static void makePluse(int a,int b) {
+		plus = a + b;
+	}
+	public static void makePluse(double a,double b) {
+		plusd = a + b;
+	}
 	public static int getPlus() {
 		return plus;
 	}
@@ -26,5 +34,11 @@ public class Compute {
 	public static double getMutipled() {
 		return mutipled;
 	}
-
+	public static int getDelive() {
+		return delive;
+	}
+	public static double getDelived() {
+		return delived;
+	}
+	
 }
--- 01Basic/src/object/Test.java
+++ 01Basic/src/object/Test.java
@@ -8,6 +8,10 @@ public class Test {
 		Vierable.chageVierable();
 		Vierable.show();
 		//以上演示变量
+		Compute.makePluse(15, 20);
+		Compute.makePluse(13.2, 19);
+		System.out.println(Compute.getPlus());
+		System.out.println(Compute.getPlusd());
 	}
 
 }
